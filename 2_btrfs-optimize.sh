1764b211adc07eb4599d44e59da8e4865bd82a04
TheRealAlexV <demine0@gmail.com>
2022-09-05 16:44:33 -0400

    btrfs script

--- 2_btrfs-optimize.sh
+++ 2_btrfs-optimize.sh
@@ -19,15 +19,40 @@ then
   sudo NONINTERACTIVE=1 ./steamos-btrfs/install.sh
 fi
 
-if [ ! -f "$LOCALBINS/compsize"  ] ||  [ ! -f "$LOCALBINS/duperemove" ] ||  [ ! -f "$LOCALBINS/rmlint" ]
-then
-	mkdir -p $SCRIPTCACHE
-  sudo pacman --noconfirm --cachedir $SCRIPTCACHE/ -Sw compsize duperemove rmlint
-  for f in $SCRIPTCACHE/*.pkg.tar.zst ; do tar -xf "$f" -C $LOCALBINS --strip-components=2 usr/bin ; done
-  rm -rf $SCRIPTCACHE
-fi
-cd /tmp || exit
-sudo rmlint --types="duplicates" --config=sh:handler=clone /home
-sudo ./rmlint.sh -d -p -r -k
-sudo rm -r rmlint*
+echo " Select the operation ************"
+echo "  1) De-duplicate"
+echo "  2) Balance and Defrag /home steamapps"
+echo "  3) Balance and Defrag sdcard steamapps"
+echo "  4) Quit" 
+read n
+
+case $n in
+        1)
+            echo "Removing Dupes"
+            if [ ! -f "$LOCALBINS/compsize"  ] ||  [ ! -f "$LOCALBINS/duperemove" ] ||  [ ! -f "$LOCALBINS/rmlint" ]
+            then
+              mkdir -p $SCRIPTCACHE
+              sudo pacman --noconfirm --cachedir $SCRIPTCACHE/ -Sw compsize duperemove rmlint
+              for f in $SCRIPTCACHE/*.pkg.tar.zst ; do tar -xf "$f" -C $LOCALBINS --strip-components=2 usr/bin ; done
+              rm -rf $SCRIPTCACHE
+            fi
+            cd /tmp || exit
+            sudo rmlint --types="duplicates" --config=sh:handler=clone /home
+            sudo ./rmlint.sh -d -p -r -k
+            sudo rm -r rmlint*            
+            ;;
+        2)
+            echo "Balance and Defrag /home steamapps"
+            sudo btrfs filesystem defrag -czstd -v -r -f /home/deck/.local/share/Steam/steamapps
+            sudo btrfs balance start -m -v /home/deck/.local/share/Steam/steamapps
+            ;;
+        3)
+            echo "Balance and Defrag sdcard steamapps"
+            sudo btrfs filesystem defrag -czstd -v -r -f /run/media/mmcblk0p1/steamapps
+            sudo btrfs balance start -m -v /run/media/mmcblk0p1/steamapps
+            ;;
+        *)
+            ;;
+esac
+
 sudo steamos-readonly enable
