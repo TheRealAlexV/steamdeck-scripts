5a87392d98ba3d211ff12c99c6ec8936e57eaa02
TheRealAlexV <demine0@gmail.com>
2022-09-06 09:00:21 -0400

    credits

--- functions/killproton.sh
+++ functions/killproton.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# From: gitlab.com/popsulfr/steamos-btrfs/
 sudo ls -l /proc/*/exe 2>/dev/null | grep -E 'wine(64)?-preloader|wineserver' | perl -pe 's;^.*/proc/(\d+)/exe.*$;$1;g;' | xargs -n 1 kill | sudo killall -s9 winedevice.exe tee
